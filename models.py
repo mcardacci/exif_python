57f663e25826506e24656509dd80ac3d50bb3529
Marco Cardacci <marco.cardacci@gmail.com>
2016-06-18 13:17:41 -0400

    adds link to postgres db

--- models.py
+++ models.py
@@ -1,19 +1,21 @@
-from app import app 
+# from app import app 
+from datetime import datetime
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.dialects import postgresql
-from sqlalchemy.dialects.postgresql import ARRAY
+from sqlalchemy.dialects.postgresql import JSON, JSONB
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://marco:mypass@localhost/exify'
 db = SQLAlchemy(app)
 
-class Picture (db.model):
+class Picture (db.Model):
     __tablename__="picture"
     id = db.Column('id', db.Integer, primary_key=True)
     title = db.Column('title', db.Unicode)
     latRef = db.Column('latRef', db.Unicode)
     longRef = db.Column('longRef', db.Unicode)
-    longitude = db.Column('longitude', ARRAY(Float))
-    latitude = db.Column('latitude', ARRAY(Float)   
+    longitude = db.Column('longitude',JSON)
+    latitude = db.Column('latitude', JSON)   
     dateStamp = db.Column('dateStamp', db.Date, default=datetime.utcnow)
 
 
